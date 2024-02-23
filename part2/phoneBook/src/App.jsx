c7b98bae9295d2c03f73b0504d7d273679f560a0
Esteban Sayago <sayagoo823@gmail.com>
2024-02-23 01:39:14 -0300

    2.6: add person to the phonebook

--- part2/phoneBook/src/App.jsx
+++ part2/phoneBook/src/App.jsx
@@ -4,14 +4,24 @@ const App = () => {
   const [persons, setPersons] = useState([
     { name: 'Arto Hellas' }
   ]) 
+
   const [newName, setNewName] = useState('')
+
+  const onChange = (event)=> {
+    setNewName(event.target.value)
+  }
+
+  const onSubmit = (event)=>{
+    event.preventDefault()
+    setPersons([...persons,{name:newName}])
+  }
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={onSubmit}>
         <div>
-          name: <input />
+          name: <input  value={newName} onChange={onChange}/>
         </div>
         <div>
           <button type="submit">add</button>
