b8ecb978cb48962c4eead3843f5ed09aeb9cac77
Esteban Sayago <sayagoo823@gmail.com>
2024-03-21 20:46:49 -0300

    exercise 3.2

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -7,6 +7,14 @@ app.get("/api/persons", (req, res) => {
   res.json(data);
 });
 
+app.get("/info", (req, res) => {
+  res.send(
+    `<h1>Phonebook has info for ${data.length} people</h1>
+    <h2>${new Date()}</h2>`
+  );
+  res.end();
+});
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
