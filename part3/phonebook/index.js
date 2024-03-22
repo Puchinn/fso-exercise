8a44b099ffb6090d41ad7c577d5a853d2b774bcd
Esteban Sayago <sayagoo823@gmail.com>
2024-03-21 22:30:10 -0300

    exercise 3.3

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -15,6 +15,16 @@ app.get("/info", (req, res) => {
   res.end();
 });
 
+app.get("/api/persons/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const person = data.find((person) => person.id === id);
+  if (person) {
+    res.json(person);
+  } else {
+    res.status(404).end();
+  }
+});
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
